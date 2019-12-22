725030e846d125b0b4d81cde454f53889df853d0
Leon <ultrafunkamsterdam@users.noreply.github.com>
2019-12-22 13:53:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,15 +10,6 @@ d88P"    888 "88b 888P"  d88""88b 888 "888 "88b d8P  Y8b d88" 888 888P"   888 88
 Y88b.    888  888 888    Y88..88P 888  888  888 Y8b.     Y88b 888 888     888  Y8bd8P  Y8b.     888
  "Y8888P 888  888 888     "Y88P"  888  888  888  "Y8888   "Y88888 888     888   Y88P    "Y8888  888   88888888
 
- 888      d8b      888      888
- 888      Y8P      888      888
- 888               888      888
- 88888b.  888  .d88888  .d88888  .d88b.  88888b.
- 888 "88b 888 d88" 888 d88" 888 d8P  Y8b 888 "88b
- 888  888 888 888  888 888  888 88888888 888  888
- 888  888 888 Y88b 888 Y88b 888 Y8b.     888  888
- 888  888 888  "Y88888  "Y88888  "Y8888  888  888
-
 BY ULTRAFUNKAMSTERDAM (https://github.com/ultrafunkamsterdam)"""
 
 from setuptools import setup
@@ -27,9 +18,9 @@ from setuptools import setup
 
 
 setup(
-    name='undetected_chromedriver',
+    name='undetected-chromedriver',
     version='1.0',
-    packages=[''],
+    packages=['undetected_chromedriver'],
     install_requires=[
         'selenium',
     ],
