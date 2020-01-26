c9708ee8306fdce47c244182ecc179f63d4d2b09
Thomas Milburn <milburn.j.thomas@gmail.com>
2020-01-26 11:46:01 +0000

    Fixed bug where webdriver property could be detected

--- undetected_chromedriver/__init__.py
+++ undetected_chromedriver/__init__.py
@@ -46,10 +46,18 @@ class Chrome:
             "Page.addScriptToEvaluateOnNewDocument",
             {
                 "source": """
-            Object.defineProperty(navigator, 'webdriver', {
-              get: () => undefined
+        Object.defineProperty(window, 'navigator', {
+            value: new Proxy(navigator, {
+              has: (target, key) => (key === 'webdriver' ? false : key in target),
+              get: (target, key) =>
+                key === 'webdriver'
+                  ? undefined
+                  : typeof target[key] === 'function'
+                  ? target[key].bind(target)
+                  : target[key]
             })
-          """
+        })
+                  """
             },
         )
         original_user_agent_string = instance.execute_script(
