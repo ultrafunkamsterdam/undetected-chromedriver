62684ac898f6a016d74294cdd4cd923870890ac0
Leon <ultrafunkamsterdam@users.noreply.github.com>
2021-01-17 06:45:20 +0100

    Update test_undetected_chromedriver.py

--- tests/test_undetected_chromedriver.py
+++ tests/test_undetected_chromedriver.py
@@ -3,7 +3,7 @@ import os
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import undetected_chromedriver as uc  # noqa
+import undetected_chromedriver.v2 as uc  # noqa
 import time  # noqa
 
 
