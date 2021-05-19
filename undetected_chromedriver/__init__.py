d0f1a2d848db7bffaa319baf133a2e13daf1af18
ultrafunkamsterdam <info@blackhat-security.nl>
2021-05-19 14:20:38 +0200

    Hello CDP Events, Bye (some) bugs

--- undetected_chromedriver/__init__.py
+++ undetected_chromedriver/__init__.py
@@ -31,7 +31,7 @@ from selenium.webdriver import Chrome as _Chrome
 from selenium.webdriver import ChromeOptions as _ChromeOptions
 
 logger = logging.getLogger(__name__)
-__version__ = "2.2.8"
+__version__ = "2.3.0"
 
 TARGET_VERSION = 0
 
@@ -72,62 +72,7 @@ class Chrome:
                                            : target[key]
                                        })
                                    });
-                                   
-                                   Object.defineProperty(window, 'chrome', {
-                                    value: new Proxy(window.chrome, {
-                                        has: (target,key) => true,
-                                        get: (target,key) => { 
-                                            return {
-                                              usingthebestantibotprotection: true
-                                              ,
-                                              app: {
-                                                isInstalled: false,
-                                              },
-                                              webstore: {
-                                                onInstallStageChanged: {},
-                                                onDownloadProgress: {},
-                                              },
-                                              runtime: {
-                                                PlatformOs: {
-                                                  MAC: 'mac',
-                                                  WIN: 'win',
-                                                  ANDROID: 'android',
-                                                  CROS: 'cros',
-                                                  LINUX: 'linux',
-                                                  OPENBSD: 'openbsd',
-                                                },
-                                                PlatformArch: {
-                                                  ARM: 'arm',
-                                                  X86_32: 'x86-32',
-                                                  X86_64: 'x86-64',
-                                                },
-                                                PlatformNaclArch: {
-                                                  ARM: 'arm',
-                                                  X86_32: 'x86-32',
-                                                  X86_64: 'x86-64',
-                                                },
-                                                RequestUpdateCheckStatus: {
-                                                  THROTTLED: 'throttled',
-                                                  NO_UPDATE: 'no_update',
-                                                  UPDATE_AVAILABLE: 'update_available',
-                                                },
-                                                OnInstalledReason: {
-                                                  INSTALL: 'install',
-                                                  UPDATE: 'update',
-                                                  CHROME_UPDATE: 'chrome_update',
-                                                  SHARED_MODULE_UPDATE: 'shared_module_update',
-                                                },
-                                                OnRestartRequiredReason: {
-                                                  APP_UPDATE: 'app_update',
-                                                  OS_UPDATE: 'os_update',
-                                                  PERIODIC: 'periodic',
-                                                },
-                                              },
-                                            }
-                                        }
-                                    })
-                                });
-                                            
+                                                                
                         """
                     },
                 )
