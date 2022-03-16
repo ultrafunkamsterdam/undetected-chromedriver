18ea1897a913e4e1b3d8a112d65ca8bd81a0071b
sebdelsol <seb.morin@gmail.com>
2022-03-16 16:44:38 +0100

    fix old uc.ChromeOptions.user_data_dir case

--- undetected_chromedriver/options.py
+++ undetected_chromedriver/options.py
@@ -43,6 +43,7 @@ class ChromeOptions(_ChromiumOptions):
         prefs = self.experimental_options.get("prefs")
         if prefs:
 
+            user_data_dir = user_data_dir or self._user_data_dir
             default_path = os.path.join(user_data_dir, "Default")
             os.makedirs(default_path, exist_ok=True)
 
