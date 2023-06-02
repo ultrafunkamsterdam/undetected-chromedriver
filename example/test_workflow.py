5c78b7cca30a0274440fb75a5f9d51caf77bb68b
Leon <ultrafunkamsterdam@users.noreply.github.com>
2023-06-02 23:56:53 +0200

    Update test_workflow.py

--- example/test_workflow.py
+++ example/test_workflow.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 import undetected_chromedriver as uc
 
 def main():
-    driver = uc.Chrome(browser_executable_path="/opt/hostedtoolcache/chromium/latest/x64/chrome", headless=True)
+    driver = uc.Chrome(headless=True)
     driver.get("https://nowsecure.nl")
     WebDriverWait(driver, 15).until(EC.text_to_be_present_in_element(("css selector", "main h1"), "OH YEAH, you passed!"))
     driver.quit()
