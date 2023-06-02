4e81361ced3754daae17a10c0d47325c0702bc97
Leon <ultrafunkamsterdam@users.noreply.github.com>
2023-06-03 00:06:25 +0200

    Update test_workflow.py

--- example/test_workflow.py
+++ example/test_workflow.py
@@ -1,13 +1,24 @@
 import time
 import logging
+import os
 logging.basicConfig(level=10)
 
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.remote.webdriver import By
 import selenium.webdriver.support.expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
+from pathlib import Path
 import undetected_chromedriver as uc
+
+# due to the randomneess of the chrome install path on the runner when running action, i have to find it manufally
+tmp = Path('/tmp')
+for item in tmp.glob('chrome*'):
+    if item.is_dir():
+        path_list = os.environ['PATH'].split(os.pathsep)
+        path_list.insert(0, str(item))
+        os.environ['PATH'] = os.pathsep.join(path_list)
+        
+        
+        
 
 def main():
     driver = uc.Chrome(headless=True)
